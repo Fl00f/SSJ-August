98c640fed1913766701f0a4e356fdc94d5ed7be2
James Robinson <deltaohio3545@yahoo.com>
2018-08-19 14:01:57 -0700

    working out draging features

---
+++ SSJ August/Assets/Scripts/TestScripts/ObjectDragTest.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(EventTrigger))]
+public class ObjectDragTest : MonoBehaviour
+{
+	public int RowInColmn => transform.GetSiblingIndex();
+	public int InColmn => transform.parent.GetSiblingIndex();
+	
+	private int swappingWithIndex;
+	
+	public int SwappingWithIndex => swappingWithIndex;
+
+	private bool isBeingDragged;
+
+	private Vector3 positionBeforeDrag;
+
+	private float Width;
+	
+	private void Start()
+	{
+		Image image = GetComponent<Image>();
+
+		Assert.IsNotNull(image);
+		
+		Width = image.rectTransform.sizeDelta.x;
+		
+		EventTrigger triggers = GetComponent<EventTrigger>();
+		
+		EventTrigger.Entry startDragEntry = new EventTrigger.Entry();
+		startDragEntry.callback.AddListener(arg0 => startDrag());
+		startDragEntry.eventID = EventTriggerType.BeginDrag;
+		
+		EventTrigger.Entry dragEntry = new EventTrigger.Entry();
+		dragEntry.callback.AddListener(arg0 => pointerDrag());
+		dragEntry.eventID = EventTriggerType.Drag;
+		
+		EventTrigger.Entry dragEndEntry = new EventTrigger.Entry();
+		dragEndEntry.callback.AddListener(arg0 => endDrag());
+		dragEndEntry.eventID = EventTriggerType.EndDrag;
+		
+		triggers.triggers.Add(startDragEntry);
+		triggers.triggers.Add(dragEntry);
+		triggers.triggers.Add(dragEndEntry);
+	}
+
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (!isBeingDragged) return;
+		
+		ObjectDragTest dragObj = other.gameObject.GetComponent<ObjectDragTest>();
+
+		if (dragObj)
+		{
+			swappingWithIndex = dragObj.RowInColmn;
+			Debug.Log(swappingWithIndex);
+		}
+		
+	}
+
+	private void startDrag()
+	{
+		positionBeforeDrag = transform.position;
+	}
+	
+	private void pointerDrag()
+	{
+		isBeingDragged = true;
+
+		float x = Input.mousePosition.x;
+		float xClamped = Mathf.Clamp(x, positionBeforeDrag.x - Width, positionBeforeDrag.x + Width);
+
+		Vector3 pos = transform.position;
+		pos.x = xClamped;
+
+		transform.position = pos;
+		
+//		Debug.Log("Is being dragged");
+	}
+
+	private void endDrag()
+	{
+		swappingWithIndex = -1;
+		isBeingDragged = false;
+
+		transform.position = positionBeforeDrag;
+//		Debug.Log("Drag End");
+
+	}
+}
--- SSJ August/Assets/Scripts/TestScripts/TestMatchThreeGrid.cs
+++ SSJ August/Assets/Scripts/TestScripts/TestMatchThreeGrid.cs
@@ -4,47 +4,102 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UI;
 
-public class TestMatchThreeGrid : MonoBehaviour {
-
-	
-	
-	
-	
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
-
-
-public class MatchThreeObject
+public class TestMatchThreeGrid : MonoBehaviour
 {
-	public Sprite SpriteVisual {
-		set
-		{
-			Assert.IsNotNull(imageHolder);
-			imageHolder.sprite = value;
-		}
-	}
+    public Transform[] gridColumns;
+    private int ColumnLength => gridColumns.Length;
 
-	public int rowInColmn;
-	
-	private int column;
+    public Vector2[,] gridImagePositions;
+    public Vector2[,] translationImageStartPositions;
 
-	public int Column => column;
 
-	private Image imageHolder;
-	
-	public MatchThreeObject(Image image, int inColumn, int rowInColmn)
-	{
-		this.imageHolder = image;
-		this.column = inColumn;
-		this.rowInColmn = rowInColmn;
-	}
+    public float translationTime;
+
+
+    #region Debug
+
+    public bool doImageTranslations;
+    public float translationStartTime;
+    
+    #endregion
+    
+    // Use this for initialization
+    void Start()
+    {
+        SetInitialGridPositions();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            translationImageStartPositions = getCurrentImagePositions();
+            translationStartTime = Time.time;
+            doImageTranslations = true;
+        }
+        
+        if (doImageTranslations && translationImageStartPositions != null)
+        {
+            float percentage = (Time.time - translationStartTime) / translationTime;
+            float percentageClamp = Mathf.Clamp(percentage, 0, 1);
+            translateImagesToGridPositions(percentageClamp);
+
+            if (percentageClamp >= 1)
+            {
+                doImageTranslations = false;
+                translationImageStartPositions = null;
+            }
+        }
+    }
+
+
+    private void translateImagesToGridPositions(float translationPercentage)
+    {
+        for (int i = 0; i < ColumnLength; i++)
+        {
+            for (int j = 0; j < gridColumns[0].childCount; j++)
+            {
+                Transform imageTransform = gridColumns[i].GetChild(j);
+
+                imageTransform.position = Vector3.Lerp(translationImageStartPositions[i, j], gridImagePositions[i, j],
+                    translationPercentage);
+            }
+        }
+    }
+
+    private Vector2[,] getCurrentImagePositions()
+    {
+        Vector2[,] currentImageStartPositions = new Vector2[ColumnLength, gridColumns[0].childCount];
+
+        for (int i = 0; i < ColumnLength; i++)
+        {
+            for (int j = 0; j < gridColumns[0].childCount; j++)
+            {
+                Vector3 pos = gridColumns[i].GetChild(j).position;
+
+                currentImageStartPositions[i, j] = new Vector2Int((int) pos.x, (int) pos.y);
+            }
+        }
+
+        return currentImageStartPositions;
+    }
+    
+    private void SetInitialGridPositions()
+    {
+        //using the pre established positions from prefab, record initial positions into grid
+        gridImagePositions = new Vector2[ColumnLength, gridColumns[0].childCount];
+
+        for (int i = 0; i < ColumnLength; i++)
+        {
+            for (int j = 0; j < gridColumns[0].childCount; j++)
+            {
+                Vector3 pos = gridColumns[i].GetChild(j).position;
+
+                gridImagePositions[i, j] = new Vector2(pos.x, pos.y);
+            }
+        }
+    }
 }
