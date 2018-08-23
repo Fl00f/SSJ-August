a278527d7cfa025e0db3561c22fd011a280eac9c
James Robinson <deltaohio3545@yahoo.com>
2018-08-22 21:32:22 -0700

    much changes

--- SSJ August/Assets/Scripts/TestScripts/ShotMoveForward.cs
+++ SSJ August/Assets/Scripts/TestScripts/ShotMoveForward.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class ShotMoveForward : MonoBehaviour {
 
+	public float moveRate = 1;
 	public bool CanHit;
 	// Update is called once per frame
 	void Update () {
-
+		transform.Translate (0, 0, moveRate * Time.deltaTime);
 	}
 }
